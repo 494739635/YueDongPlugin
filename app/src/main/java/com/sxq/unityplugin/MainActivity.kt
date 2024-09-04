5eedd3f785b3ac08c80c0d88d6b965f4b0dd5f4e
shuxq <shuxiaoqiang@shandiantech.com>
2024-09-04 18:08:15 +0800

    feat:腾讯云的推拉流

--- app/src/main/java/com/sxq/unityplugin/MainActivity.kt
+++ app/src/main/java/com/sxq/unityplugin/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.sxq.unityplugin
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
 import androidx.activity.ComponentActivity
 import com.live.ss.R
+import com.sxq.unityplugin.ui.play.LebPlayEnterActivity
+import com.sxq.unityplugin.ui.push.LivePushCameraEnterActivity
+import com.yuedong.plugin.LivePreviewActivity
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<View>(R.id.ll_push_camera).setOnClickListener {
+            val intent =
+                Intent(this@MainActivity, LivePushCameraEnterActivity::class.java)
+            startActivity(intent)
+        }
+
+        findViewById<View>(R.id.ll_leb_play).setOnClickListener {
+            val intent = Intent(this@MainActivity, LebPlayEnterActivity::class.java)
+            startActivity(intent)
+        }
+
+        findViewById<View>(R.id.ll_leb_beauty).setOnClickListener {
+            val intent = Intent(this@MainActivity, LivePreviewActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
