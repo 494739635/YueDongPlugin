83635e072974ebeeeba037d66fe234a6d6b6209c
shuxq <shuxiaoqiang@shandiantech.com>
2024-09-05 17:03:58 +0800

    feat:提供测试方法

--- plugin/src/main/java/com/yuedong/plugin/PluginManager.kt
+++ plugin/src/main/java/com/yuedong/plugin/PluginManager.kt
@@ -1,5 +1,11 @@
 package com.yuedong.plugin
 
-class PluginManager {
+import android.content.Context
+import android.widget.Toast
 
+object PluginManager {
+
+    fun showToastForAndroid(context: Context) {
+        Toast.makeText(context, "Hello UNI", Toast.LENGTH_LONG).show()
+    }
 }
